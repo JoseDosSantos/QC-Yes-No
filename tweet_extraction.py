3541d7aceb4d615eb775e173cf45646dd3483515
Josef Cersovsky <jcersovsky@kpmg.com>
2018-06-21 19:31:43 +0200

    added tkinter

--- tweet_extraction.py
+++ tweet_extraction.py
@@ -2,7 +2,7 @@ import tweepy
 import re
 import nltk
 
-nltk.data.path.append('C:\\Users\\jcersovsky\\PycharmProjects\\QC\\nltk_data')
+nltk.data.path.append('C:\\Users\\jcersovsky\\Documents\\Projects\\Bachelorarbeit\\nltk_data')
 
 consumer_key = "363klSkj4Eb6cy36ZFwc1zLwN"
 consumer_secret = "xSnNdpeicNyGmK7mredAK0pCKhoTUOX135bcv1ksxgKAtg5n7h"
@@ -16,13 +16,25 @@ auth.set_access_token(access_token, access_token_secret)
 # Creating the API object while passing in auth information
 api = tweepy.API(auth)
 
+if (not api):
+    print("Can't Authenticate")
+    exit(-1)
+
+
 # The search term you want to find
 query = "?"
 # Language code (follows ISO 639-1 standards)
 language = "de"
+# Total number of tweets
+maxTweets = 1000
+# This is the max the API permits
+tweetsPerQry = 100
+# Name of the output file
+fName = 'tweets.txt'
+
 
 # Calling the user_timeline function with our parameters
-results = api.search(q=query, lang=language, count=200, show_user=False, tweet_mode='extended')
+#results = api.search(q=query, lang=language, count=maxTweets, show_user=False, tweet_mode='extended')
 
 
 
@@ -41,17 +53,51 @@ def clean_tweet(original_tweet):
     tweet = tokenize_sentences(tweet)
     return tweet
 
+tweetCount = 0
+sinceId = None
+max_id = -1
+
+with open(fName, 'w', encoding="utf-8") as f:
+    while tweetCount < maxTweets:
+        try:
+            if (max_id <= 0):
+                if (not sinceId):
+                    new_tweets = api.search(q=query, count=tweetsPerQry, lang=language)
+                else:
+                    new_tweets = api.search(q=query, count=tweetsPerQry, lang=language, since_id=sinceId)
+            else:
+                if (not sinceId):
+                    new_tweets = api.search(q=query, count=tweetsPerQry, lang=language, max_id=str(max_id - 1))
+                else:
+                    new_tweets = api.search(q=query, count=tweetsPerQry, lang=language, max_id=str(max_id - 1), since_id=sinceId)
+            if not new_tweets:
+                print("No more tweets found")
+                break
+            for tweet in new_tweets:
+                for sent in clean_tweet(tweet._json['text']):
+                    if '?' in sent:
+                        f.write(sent + '\n')
+
+            tweetCount += len(new_tweets)
+            print("Downloaded {0} tweets".format(tweetCount))
+            max_id = new_tweets[-1].id
+        except tweepy.TweepError as e:
+            # Just exit if any error
+            print("some error : " + str(e))
+            break
+pass
 
 
-# foreach through all tweets pulled
-with open("Output.txt", "w", encoding="utf-8") as text_file:
-    for tweet in results:
-        cleaned_tweet = clean_tweet(tweet._json['full_text'])
-        # printing the text stored inside the tweet object
-        for sent in cleaned_tweet:
-            if '?' in sent:
-                print(sent + '\n', file=text_file)
-        print('\n' + 20*'*' + '\n', file=text_file)
+
+# # foreach through all tweets pulled
+# with open(fName, "w", encoding="utf-8") as text_file:
+#     for tweet in results:
+#         cleaned_tweet = clean_tweet(tweet._json['full_text'])
+#         # printing the text stored inside the tweet object
+#         for sent in cleaned_tweet:
+#             if '?' in sent:
+#                 print(sent + '\n', file=text_file)
+#         print('\n' + 20*'*' + '\n', file=text_file)
 
 
 
